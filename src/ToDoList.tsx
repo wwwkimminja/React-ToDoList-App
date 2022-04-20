9abc24e6312eade1235a01ff107549a92d853266
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-04-20 19:04:49 +0900

    Using recoil at ToDoList

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,110 +1,76 @@
 import React from "react";
 import {useForm} from 'react-hook-form';
 import styled from 'styled-components';
+import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
 
-const Form = styled.form`
-width: 80%;
-height:300px;
+const Container = styled.div`
     display: flex;
     flex-direction: column;
+    align-content: center;
+    padding: 10px;
+`
+const Form = styled.form`
+width: 80%;
+    display: flex;
     align-items: center;
+    justify-content: center;
    
-    margin:10px;
-    padding :0;
-
-    
-    input{
-    
-       width: 90%;
-       margin-top: 5px;
+    input {
+        width: 80%;
     }
     button {
-        width:93%;
-        margin-top: 15px;
+        width: 20%;
+        margin:10px;
     }
-    span{
-        margin-left: 10px;
-        align-self: flex-start;
-        color:red;
-        font-size: 0.5rem;
+   
+    li{
+       margin:10px;
     }
 `;
 
-
-/*
-function ToDoList(){
-     const [toDo,setTodo]= useState("");
-
-    const onChange = (event:React.FormEvent<HTMLInputElement>)=>{
-        const {currentTarget:{value},}=event;
-        setTodo(value);
-    }
-
-    const onSubmit =(event:React.FormEvent<HTMLFormElement>)=>{
-        event.preventDefault();
-        
-    }
-  return (
-      <div>
-          <form onSubmit={onSubmit}>
-              <input onChange={onChange} value={toDo} placeholder="WriteHere"></input>
-              <button>Add</button>
-          </form>
-      </div>
-  )
+interface IToDo {
+    text:string;
+    id: number,
+    category:"TO_DO"| "DOING" | "DONE";
 }
- */
+const toDoState = atom<IToDo[]>({
+    key:"toDo",
+    default:[],
+
+})
+
 
 interface IForm {
-    email: string;
-    firtstName: string;
-    lastName: string;
-    username: string;
-    password: string;
-    password1: string;
-    extraError?:string;
+    toDo:string;
+ 
+ 
 }
 function ToDoList(){
-    const {register, handleSubmit, formState:{errors} ,setError}=useForm<IForm>({
-        defaultValues:{
-            email:"@gmail.com",
-        },
+    const [toDos,setToDos]= useRecoilState(toDoState);
+    //const value = useRecoilValue(toDoState);
+    //const modFn = useSetRecoilState(toDoState);
+   const { register, handleSubmit,setValue} = useForm<IForm>();
+   const handleValid=({toDo}:IForm)=>{
+       setToDos(oldToDos => [{text:toDo,id:Date.now(),category:"TO_DO"},...oldToDos])
+       setValue("toDo","")
+   }
+   console.log(toDos);
 
-    });
-    const onValid = (data:IForm) => {
-        if(data.password !== data.password1){
-            setError("password1",{message:"Password are not the same!!"},{shouldFocus:true})
-        }
-        setError("extraError",{message:"Server offline"});
 
-    }    
     return(
-        <div>
-            <Form onSubmit={handleSubmit(onValid)}>
-                <input {...register("email",
-                {required:"Email Required",
-              
-                pattern:{value:/^[A-Za-z0-9._%+-]+@gmail.com$/,
-                message:"Only gmail.com emails allowed"},})} placeholder="Email"/>
-                <span>{errors?.email?.message}</span>
-                <input {...register("firtstName",{required:"first name is required" , minLength:{value:10,message:"Your name is too Shot"} })} placeholder="First Name"/>
-                <span>{errors?.firtstName?.message}</span>
-                <input {...register("lastName",{
-                    required:"last name is required",
-                    validate:{
-                        noNico:(value) => value.includes("nico")? "no nico allowed ":true,
-                        noNick:(value) => value.includes('nick')?"no nick allowed":true,}})} placeholder="Last Name"/>
-                <span>{errors?.lastName?.message}</span>
-                <input {...register("username",{required:"username is required"})} placeholder="User Name"/>
-                <span>{errors?.username?.message}</span>
-                <input {...register("password",{required:"password is required"})} placeholder="Password"/>
-                <span>{errors?.password?.message}</span>
-                <input {...register("password1",{required:"password1 is required"})} placeholder="Passward1"/>
-                <span>{errors?.password1?.message}</span>
+        <Container>
+            <h1>To Dos</h1>
+            
+            <Form onSubmit={handleSubmit(handleValid)}>
+                <input {...register("toDo",{
+                    required:"Please Write a To Do"
+                })} placeholder="Please Write a To Do"/>
                 <button>Add</button>
-                <span>{errors?.extraError?.message}</span>
             </Form>
-        </div>
+            <ul>
+                {toDos.map(toDo =><li key={toDo.id}>{toDo.text}</li>)}
+            </ul>
+        </Container>
     )
 }
 
