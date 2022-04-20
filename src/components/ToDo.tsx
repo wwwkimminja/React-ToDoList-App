f1e170edcb33313cbba184d85a1149ae7c6b7ff8
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-04-20 21:26:51 +0900

    Update category

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,20 +1,38 @@
 import { IToDo } from "../atoms";
 import React from "react";
+import { useSetRecoilState } from "recoil";
+import { toDoState } from "../atoms";
 
 
 
 function ToDo ({text,category,id}:IToDo) {
-    const onClick = (event:React.MouseEvent<HTMLButtonElement>)=>{
-       const {currentTarget:{name}} = event;
-       console.log("I wanna to ",name);
+    const setToDos = useSetRecoilState(toDoState);
+    // const onClick = (event:React.MouseEvent<HTMLButtonElement>)=>{
+    //    const {currentTarget:{name}} = event;
+    //    setToDos((oldToDos) => {
+    //        const targetIndex = oldToDos.findIndex(toDo => toDo.id ===id)
+    //        const oldToDo = oldToDos[targetIndex];
+    //        const newToDo ={text,id,category:name as any}; *** any 사용하지 않도록****
+
+
+    //        return [...oldToDos.slice(0,targetIndex),newToDo,...oldToDos.slice(targetIndex+1)];
+    //    })
+    // };
+    const onClick =(newCategory:IToDo["category"])=>{
+        setToDos((oldToDos)=>{
+            const targetIndex = oldToDos.findIndex(toDo => toDo.id ===id)
+                  const oldToDo = oldToDos[targetIndex];
+                  const newToDo ={text,id,category:newCategory};
+                  return [...oldToDos.slice(0,targetIndex),newToDo,...oldToDos.slice(targetIndex+1)];
+        })
     };
 
     return (
         <li >
             <span>{text}</span> 
-            {category !=="DOING" && <button name="DOING" onClick={onClick} >Doing</button>}
-            {category !=="TO_DO" && <button name="TO_DO" onClick={onClick} >To Do</button>}
-            {category !=="DONE" && <button name="DONE" onClick={onClick} >Done</button>}
+            {category !=="DOING" && <button  onClick={()=>onClick("DOING")} >Doing</button>}
+            {category !=="TO_DO" && <button  onClick={()=>onClick("TO_DO")} >To Do</button>}
+            {category !=="DONE" && <button onClick={()=>onClick("DONE")} >Done</button>}
         </li>
     );
 }
